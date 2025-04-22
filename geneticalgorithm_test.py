02f42335cf91d258b14cc760643723385c2716ef
Shen Ge <shenge86@gmail.com>
2025-04-21 21:27:20 -0500

    GA: add initial genetic algorithm baseline
    Add initial algorithm test

---
+++ geneticalgorithm_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 20:36:40 2025
+
+@author: Shen Ge
+@name: Genetic Algorithm Tester
+"""
+
+if __name__ == '__main__':
+    print('Genetic Algorithm Tester')
+    
---
+++ randomrunscript.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 21:26:11 2025
+
+@author: Shen Ge
+@name: Random Run Script
+"""
+
