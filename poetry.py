1293447cec1ca5ee116ff5c26215969803e3e454
Shen Ge <shenge86@gmail.com>
2026-02-01 01:58:22 -0600

    Image: add images and update script to open up appropriate images going with words

--- poetry.py
+++ poetry.py
@@ -4,10 +4,20 @@ Created on Wed Dec 24 19:40:42 2025
 
 @author: sheng
 @name: Generates random poetry
+@description:
+    
+    Generates words that make some sense and images that go with it
 """
 import os, sys
 import pickle
 from datetime import datetime
+
+import random
+from pathlib import Path
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
 from wonderwords import RandomWord, RandomSentence
 
 if __name__ == '__main__':
@@ -39,26 +49,25 @@ if __name__ == '__main__':
         # TYPE 2
         bodyparts = ['body',
                      'breast',
-                     'butt'
+                     'butt',
                      'eyes',
                      'face',
+                     'feet',
                      'hair',
                      'legs',
-                     'nipples',
                      'pussy']
         
         models = ['Allie Haze',
                   'Camryn',
-                  'Ceara Lynch',
-                  'Emma Kuziara', 
+                  'Emma Kuziara',
+                  'Kylie Page',
                   'Leanna Lovings',
-                  'Lexi Belle',
-                  'Melanie Rios']
+                  'Sapphire Blue',]
         
         actions= ['eyeballing',                  
-                  'imagining making love to',
-                  'pleasuring oneself to',
-                  'salivating over',
+                  'imagining in bed with',
+                  'pleasing oneself with thoughts of',
+                  'starstruck over',
                   'watching',
                   'worshiping']
         
@@ -81,11 +90,16 @@ if __name__ == '__main__':
         sentences.append(s.simple_sentence())
         sentences.append(s.sentence())
         
+        
+        # saved up generator for image display as well
+        bodypart = generator_x.word(include_categories=['bodypart'])
+        model    = generator_x.word(include_categories=['model'])
+        
         line000 = 'The lines above are mechanically created with uncuration except in adjective, verb and noun formation'
         line001 = 'What follows is more deliberate.'
         line01 = 'A ' + generator.word(include_categories=['human']) + ' loves to ' + generator.word(include_categories=['sexual']) + ' ' + generator.word(include_categories=['fruit'])        
         line02 = f"Yet, to a {generator.word(include_categories=['human'])}, nothing feels quite as good as spending hours"
-        line03 = 'staring at a screen and ' + generator_x.word(include_categories=['action']) + ' the ' + generator_x.word(include_categories=['bodypart']) + ' of ' + generator_x.word(include_categories=['model'])
+        line03 = 'staring at a screen and ' + generator_x.word(include_categories=['action']) + ' the ' + bodypart + ' of ' + model
         line04 = 'The euphoria may be ' + generator_time.word() + ' and the shame may be ' + generator_time.word()
         line05 = 'The cycle repeats...'
         
@@ -103,12 +117,12 @@ if __name__ == '__main__':
             'verbs': [verb1],            
             'others': others,
             'sentences': sentences,
+            'bodypart': bodypart,
+            'model': model,
             }
     #%%            
     result = ' '.join([words['adjectives'][0], words['nouns'][0], words['verbs'][0]])
     result2 = ' '.join([words['others'][1], words['others'][2]])
-    
-    
     
     lines_to_save = [
         words['others'][0],
@@ -125,6 +139,37 @@ if __name__ == '__main__':
     for line in lines_to_save:
         print(line)
 
+    #%% Image opener
+    # img = Image.open('data_img/emmak/emma-k-13.jpg') # if not using random
+    
+    # random image from the folder
+    if model in ['Allie Haze']:
+        image_dir = Path('data_img/allieh')
+    elif model in ['Camryn']:
+        image_dir = Path('data_img/camryn')
+    elif model in ['Emma Kuziara']:
+        image_dir = Path("data_img/emmak")
+    elif model in ['Kylie Page']:
+        image_dir = Path("data_img/kyliep")
+    elif model in ['Leanna Lovings']:
+        image_dir = Path("data_img/leannal")
+    elif model in ['Sapphire Blue']:
+        image_dir = Path("data_img/sapphireb")
+    
+    images = list(image_dir.glob("*.jpg")) + list(image_dir.glob("*.png"))
+    if not images:
+        raise ValueError("No images found in folder")
+
+    img_path = random.choice(images)
+    
+    # open up the image
+    img = Image.open(img_path)
+    plt.imshow(img)
+    plt.axis("off")                 # hide axes
+    plt.tight_layout(pad=0)
+    plt.show()
+
+    #%% Write the poetry to a text file and save the words to a word database
     os.makedirs('poetry', exist_ok=True)    
     current_datetime = datetime.now()
     filename = current_datetime.strftime("%Y-%m-%d_%H-%M-%S.%f") + ".txt"
@@ -132,8 +177,7 @@ if __name__ == '__main__':
     
     with open(filepath, 'w') as f:
         f.writelines(lines_to_save)
-        
-    #%%
+    
     print('Word database saved: ', words)
     with open(file_path, 'wb') as file:
         pickle.dump(words, file)
